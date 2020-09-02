753f2245a43ac684e314058208a71b0b93e2f67d
Aria Love <aria.love.720@gmail.com>
2020-09-01 22:07:39 -0600

    Added cube w/ MoverScript

---
+++ New Unity Project/Assets/MoverScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class MoverScript : MonoBehaviour
+{
+    public float speed = 3f;
+    public float x, y, z;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log( message: "Hello World!");
+   }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        y = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        z = 0;
+        transform.Translate(x, y, z);
+    }
+}
