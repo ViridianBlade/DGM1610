c0b2ae497e241407d585e78c98495f0098a5ae1f
Aria Love <aria.love.720@gmail.com>
2020-09-03 17:46:17 -0600

    Added PlayerScore script

--- New Unity Project/Assets/PlayerScore.cs
+++ New Unity Project/Assets/PlayerScore.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
+
+    public int score = 0;
+    public Text scoreDisplay;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreDisplay = GetComponent<Text>();
     }
 
     // Update is called once per frame
-    void Update()
+
+    public void updateScore(int scoreChange)
     {
-        
+        score += scoreChange;
+        scoreDisplay.text = "Score: " + score.ToString();
     }
 }
