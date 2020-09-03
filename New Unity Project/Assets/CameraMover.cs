1a0e89cbf1aa6bef6e292999ff568a09cabcbbe3
Aria Love <aria.love.720@gmail.com>
2020-09-03 16:43:26 -0600

    Implemented 3rd person mouse look

---
+++ New Unity Project/Assets/CameraMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    
+    public float vLookSensitivity = 300f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+        
+
+        // Update is called once per frame
+    void Update()
+    {
+        var vLookInput = vLookSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        var point = transform.parent.position;
+        var axis = transform.parent.right;
+        
+        transform.RotateAround( point, axis, vLookInput);
+    }
+}
