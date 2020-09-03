5042fddbc51bf795ac8a4053ddf36d37ddcb7861
Aria Love <aria.love.720@gmail.com>
2020-09-03 17:21:16 -0600

    Move character controls to CharacterMover

--- New Unity Project/Assets/CharacterMover.cs
+++ New Unity Project/Assets/CharacterMover.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
+    public float speed = 3f;
+    public float lookSensitivity = 300f;
+    public int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log( message: "Hello World!");
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    // Update is called once per frameS
     void Update()
     {
         
+
+        var vInput = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        var hInput = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        var movement = new Vector3(hInput,0, vInput);
+        
+        
+        var hLookInput = lookSensitivity * Input.GetAxis("Mouse X") * Time.deltaTime;
+        
+        var lookDelta = new Vector3(0, hLookInput,0);
+        
+        transform.Rotate(lookDelta);
+        transform.Translate(movement);
+    }
+
+    public void Up()
+    {
+        transform.Translate(0, speed, 0);
     }
 }
